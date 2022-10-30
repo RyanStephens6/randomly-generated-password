497d7d82141b256ad9eb7f910f1538f81245a4ec
Ryan Stephens <ryandstephens6@gmail.com>
2022-10-30 14:53:40 -0600

    Reworked password attribute selector from checkboxes to promts and confirmations

--- Develop/script.js
+++ Develop/script.js
@@ -6,21 +6,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
-  //Display checkboxes for selecting password traits once button is pressed
-  var prompts = document.getElementById("prompts");
-  var promptsDisplay = getComputedStyle(prompts).display;
-  if (promptsDisplay == "none") {
-    prompts.style.display = "inline";
-  } else {
-    prompts.style.display = "none";
+  //Asks for users desired password length
+  let passwordLength = prompt("Please enter the number of characters you wish your password to be. (8-24)");
+  //Resets function if password length is not 8-24
+  if (passwordLength < 8 || passwordLength > 24) {
+    alert("Please enter a valid password length.")
+    return;
   }
 
+  //Asks user if they want lowercase, uppercase, numeric, and/or special characters
+  let lowerCase = confirm("Do you want your password to contain lowercase characters?");
+  let upperCase = confirm("Do you want your password to contain uppercase characters?");
+  let numeric = confirm("Do you want your password to contain numbers?");
+  let special = confirm("Do you want your password to contain special characters? (i.e. @ or &");
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword(lowerCase, upperCase, numeric, special) {
+
 }
 
 // Add event listener to generate button
